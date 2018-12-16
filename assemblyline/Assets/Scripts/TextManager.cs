b281f74e4be5f141185343848ffb1a38adae1b15
nichow <nhgarcia27@gmail.com>
2018-12-16 14:09:23 -0500

    Add scene where a foreman figure is talking to workers Allow scene to lead into main gameplay scene

---
+++ assemblyline/Assets/Scripts/TextManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TextManager : MonoBehaviour 
+{
+
+	public Text TextBox; // the text unity object
+	private int _textIndex = 0;
+
+	/* The text available to be displayed in the scene */
+	private readonly string[] _text =
+	{
+		"I am giving you instructions.",
+		"I am clarifying those instructions.",
+		"I am making a snide remark."
+	};
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(AnimateText());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Submit"))
+		{
+
+			if (_textIndex == _text.Length - 1) SceneManager.LoadScene("AssemblyLineScene");
+			_textIndex++;
+			StopAllCoroutines();
+			StartCoroutine(AnimateText());
+		}
+	}
+	
+	IEnumerator AnimateText()
+	{
+		for (var i = 0; i < _text[_textIndex].Length + 1; i++)
+		{
+			TextBox.text = _text[_textIndex].Substring(0, i);
+			yield return new WaitForSeconds(.01f);
+		}
+	}
+}
